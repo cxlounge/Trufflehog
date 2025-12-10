61df7dbeaeb38b0a6c9472f3a327863bb3b2f263
Frederic Malo <frederic.malo@checkmarx.com>
2025-12-10 16:09:30 +0100

    medium entropy

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-my_var = "NhAAAAAwEAAQAAAYEA6XGeS8qF7EQJVY9tWsIYoWW2xQZyP+++fP4rBmMor4FOcf8V+euz"
+my_critical_entropy = "NhAAAAAwEAAQAAAYEA6XGeS8qF7EQJVY9tWsIYoWW2xQZyP+++fP4rBmMor4FOcf8V+euz"
 
+my_medium_entropy = "myproject-3F2504E0-4F89-11D3-9A0C-0305E82C3301"
+
+my_low_entropy = "user12345"
